7c2d667807f51fe5435187f598078f6fed2f0066
Matthew Banister <matthew.banister@gmail.com>
2015-09-22 23:39:20 -0400

    BFS in clojure

--- clojure/src/hackerrank/graph.clj
+++ clojure/src/hackerrank/graph.clj
@@ -34,27 +34,39 @@
 (defn next-level [graph src]
   (vec (set (flatten (map graph (src graph))))))
 
+(defn print-result [graph start-node nodes]
+  (let [res
+        (map
+         #(if (> % 0) (* 6 %) %)
+         (filter
+          #(not (= % 0))
+          (map
+           #(find-depth graph (keyword start-node) (keyword (str %)))
+           (range 1 (inc nodes)))))]
+    (doseq [r res] (print (str r " "))) (println "")))
+
+(defn parse-edges [edge-lines]
+  (map
+   (fn [edge-line]
+     (-> edge-line
+         (str)
+         (#(s/trim %))
+         (#(s/split % #" "))
+         (#(map keyword %))
+         (vec)))
+   edge-lines))
+
 (defn -main []
-  (let [num-test-cases (Integer/parseInt (s/trim (read-line)))]
-    (doseq [i (range num-test-cases)]
-      (let [edges-and-nodes (s/split (s/trim (read-line)) #" ")
+  (let [lines (line-seq (java.io.BufferedReader. *in*))
+        num-test-cases (Integer/parseInt (s/trim (first lines)))]
+    (loop [x num-test-cases
+           remaining (rest lines)]
+      (let [edges-and-nodes (s/split (s/trim (str (first remaining))) #" ")
             nodes (Integer/parseInt (first edges-and-nodes))
             edges (Integer/parseInt (last edges-and-nodes))
-            edge-list (reduce
-                       (fn [acc line]
-                         (let [line-parsed (map keyword (s/split (s/trim (read-line)) #" "))]
-                           (into acc [line-parsed])))
-                       []
-                       (range edges))
-            start-node (s/trim (read-line))
-            g (build-graph nodes edge-list)
-            res (map
-                 #(if (> % 0) (* 6 %) %)
-                 (filter
-                  #(not (= % 0))
-                  (map
-                   #(find-depth g (keyword start-node) (keyword (str %)))
-                   (range 1 (inc nodes)))))]
-        (doseq [r res]
-          (print (str r " ")))
-        (println "")))))
+            edge-list (parse-edges (take edges (rest remaining)))
+            start-node (keyword (s/trim (apply str (take 1 (drop (inc edges) remaining)))))
+            g (build-graph nodes edge-list)]
+        (print-result g start-node nodes)
+        (when (> num-test-cases 1)
+          (recur (dec num-test-cases) (drop (+ 2 edges) remaining)))))))
